f6959c7d46e334575e1e65a8f6d9bf1e6af5bffe
OliGanguly <82031303+OliGanguly@users.noreply.github.com>
2023-05-08 15:27:44 -0400

    oli

--- App.js
+++ App.js
@@ -1,26 +1,46 @@
-// Here we Write all my React code
 
-    // Just create a h1 element
-    //{} where you give attributes to your tag
-    const heading = React.createElement("h1",
+import React from 'react';
+import { ReactDOM } from 'react-dom';
+// Just create a h1 element
+//{} where you give attributes to your tag
+const heading1 = React.createElement("h1",
     {
-    id:"heading",
+        id: "heading",
     },
     "Hello world from React");
-    //  console.log(heading)its a Object so React.createElement is a Object
-    // Craete a Root where we do all dom Manupulation, 
-    // root is the place where all the react code will run
-    // Now we have to put the h1 into DOM
+//  console.log(heading)its a Object so React.createElement is a Object
+// Craete a Root where we do all dom Manupulation, 
+// root is the place where all the react code will run
+// Now we have to put the h1 into DOM
 
-    const root=ReactDOM.createRoot(document.getElementById("root"));
-    // render will take this object make it h1 tag which broswer understand and put it in root
-    root.render(heading)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// render will take this object make it h1 tag which broswer understand and put it in root
+root.render(heading1)
 
-    /*
-    
-    <div>
-    <div>
-    <h1>Oli</h1>
-    </div>
-    </div>
-    */
+/*Lot of other packeges we need here to made aproduction ready app in React
+npm-does not have full form . it manages packages . Standard repo for all the packages all packages are hoisted over here.
+terminal->npm init -> provide details->package.json
+so package.json is configuration for npm
+packages also known as dependency, npm takes care of that like version in package.json
+*/
+//    -----------------------------------------
+//  Bundler->Webpack use to bundle or compressed our code to make it production Ready
+/*
+create-react-app behind it uses webpack to bundle all code
+How to get parcel(Bundler) in our app
+npm i -D parcel //D for we want to install it as Dev Dependency
+npx parcel index.html->hosted on localhost:1234 port
+*/
+/*
+npm init-to generate package.json
+package.json is configuration of npm,keeps track of all the versions of dependencies with
+^(minor upgradation and ~ for major upgradation)
+pacakage-lock.json - keeps track of exact version of dependencies
+node-modules contaions code of dependencies each dependency has own package.json
+*/
+/*
+Use parcel to ignite our app npx(execute) parcel index.html
+npx->Execute and npm->install
+*/
+
+
---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,36 @@
+// Here we Write all my React code
+// Just create a h1 element
+//{} where you give attributes to your tag
+const heading = React.createElement("h1", {
+    id: "heading"
+}, "Hello world from React");
+//  console.log(heading)its a Object so React.createElement is a Object
+// Craete a Root where we do all dom Manupulation, 
+// root is the place where all the react code will run
+// Now we have to put the h1 into DOM
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// render will take this object make it h1 tag which broswer understand and put it in root
+root.render(heading) /*Lot of other packeges we need here to made aproduction ready app in React
+npm-does not have full form . it manages packages . Standard repo for all the packages all packages are hoisted over here.
+terminal->npm init -> provide details->package.json
+so package.json is configuration for npm
+packages also known as dependency, npm takes care of that like version in package.json
+*/  //    -----------------------------------------
+ //  Bundler->Webpack use to bundle or compressed our code to make it production Ready
+ /*
+create-react-app behind it uses webpack to bundle all code
+How to get parcel(Bundler) in our app
+npm i -D parcel //D for we want to install it as Dev Dependency
+npx parcel index.html->hosted on localhost:1234 port
+*/  /*
+npm init-to generate package.json
+package.json is configuration of npm,keeps track of all the versions of dependencies with
+^(minor upgradation and ~ for major upgradation)
+pacakage-lock.json - keeps track of exact version of dependencies
+node-modules contaions code of dependencies each dependency has own package.json
+*/  /*
+Use parcel to ignite our app npx(execute) parcel index.html
+npx->Execute and npm->install
+*/ ;
+
+//# sourceMappingURL=index.6bd02f5a.js.map
